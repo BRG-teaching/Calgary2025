85f77ec7e6ff6af8a271765c740dc13053648426
Li Chen <leochen1990@gmail.com>
2025-02-11 11:50:38 -0700

    clean up boolean

--- 200_compas_introduction/211_booleans.py
+++ 200_compas_introduction/211_booleans.py
@@ -2,18 +2,15 @@
 # venv: brg-csd
 # r: compas_rv
 
-from compas.colors import Color
-from compas.datastructures import Mesh
 from compas.geometry import Box
 from compas.geometry import Sphere
-from compas.geometry import Polygon
-from compas.geometry import boolean_difference_mesh_mesh
 from compas.scene import Scene
 
 # Create a box and a sphere
 box = Box(2)
 sphere = Sphere(radius=1.0, point=[1, 1, 1])
 result = box.to_brep() - sphere.to_brep()
+print(result)
 
 # Create a scene
 scene = Scene()
